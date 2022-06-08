bb22758ee23367bbc2bd29cc1df2618ebdce32e8
Uchkun <85468493+uchkun-ux@users.noreply.github.com>
2022-06-08 14:58:36 +0500

    Test
    Commentdagi kodni o`chirdim, profilimda kod review qismi o`zgararmikin ko`rmoqchiman, uzr

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
-# Create your views here.
+
 
 class PostView(APIView):
     permission_classes = (AllowAny,)
